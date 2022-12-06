ad5542b9fe55553895b9dcc50ddc96a48f266f52
RenatoJnior <108882508+RenatoJnior@users.noreply.github.com>
2022-12-06 13:09:23 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('teste 123');
+});
+app.listen(PORT, HOST);
